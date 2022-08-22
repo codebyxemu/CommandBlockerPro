d495020203b99d7ad576b40d4727a32c8c0baabb
Magnus K <74688700+codebyxemu@users.noreply.github.com>
2022-08-22 16:35:24 +0200

    Project Init 2

---
+++ src/main/java/me/xemu/commandblockerpro/CommandBlocker.java
@@ -0,0 +1,17 @@
+package me.xemu.commandblockerpro;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CommandBlocker extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
